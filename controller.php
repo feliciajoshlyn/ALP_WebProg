90c4318f6123cae8a67059155bd110a699069883
feliciajoshlyn <feliciajoshlyn@gmail.com>
2024-05-28 15:33:43 +0700

    controller added

---
+++ controller.php
@@ -0,0 +1,48 @@
+<?php
+    //function to connect to database
+    function my_connectDB(){
+        $host = "localhost";
+        $user = "root";
+        $pwd = "";
+        $db = "jasa_titip";
+
+        $conn = mysqli_connect($host, $user, $pwd, $db) or die("Error connecting to database");
+        return $conn;
+    }
+
+    //function to close connection to database
+    function my_closeDB($conn){
+        mysqli_close($conn);
+    }
+
+    //function to insert to user database
+    function saveUser($username, $password, $telnum, $email, $address){
+        $conn = my_connectDB();
+
+        $sql = "INSERT INTO customers (username, password, telephone_num, email, address) VALUES ($username, $password, $telnum, $email, $address)";
+        $result = mysqli_query($conn, $sql);
+
+        my_closeDB($conn);
+    }
+
+    //function to update user info
+    function updateUser(){
+        $conn = my_connectDB();
+
+        $sql = "";
+        $result = mysqli_query($conn, $sql);
+
+        my_closeDB($conn);
+    }
+
+    //function to delete user from database
+    hello
+    function deleteUser(){
+        $conn = my_connectDB();
+
+        $sql = "";
+        $result = mysqli_query($conn, $sql);
+
+        my_closeDB($conn);
+    }
+?>
---
+++ register.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <h1>Login Register</h1>
+    <form method="POST" action="">
+        Username: <input type="text" name="username"><br>
+        Email: <input type="text" name="email" placeholder="example@gmail.com"><br>
+        Password: <input type="password" name="password">
+        Phone number:<input type="text" name="phone-input" id="phone-input" aria-describedby="helper-text-explanation" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="123-456-7890" required /><br>
+        Address: <input type="text" name="address">
+    </form>
+</body>
+</html>
